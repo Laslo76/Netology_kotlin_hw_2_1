bad90c356a8ea702c1322641aeeef2fb92062aa6
laslo76 <bogushev.v@yandex.com>
2026-01-02 12:40:55 +0300

    result convert to commission

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,9 +2,9 @@ fun main() {
     val taxRate: Double = 0.75 // Ставка в %
     val minPayment: Int = 35 // минимальная комиссия
 
-    val amount: Int = 5000 // сумма перевода в рублях
+    val amount: Int = 500 // сумма перевода в рублях
 
-    var result: Int = (amount * taxRate / 100).toInt()
-    result = if (result < minPayment) minPayment else result
-    println("Сумма комиссии за перевод: $result руб.")
+    val commission: Int = (amount * taxRate / 100).toInt()
+
+    println("Сумма комиссии за перевод: ${if (commission < minPayment) minPayment else commission} руб.")
 }
